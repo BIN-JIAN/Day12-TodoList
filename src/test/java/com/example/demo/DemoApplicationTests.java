2fbbea615b4da67289f26760cf7e006bb8818c46
BIN <BIN.JIAN@OOCL.COM>
2025-09-16 18:07:54 +0800

    test: add test for update todo

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,16 +2,21 @@ package com.example.demo;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.demo.repository.dao.TodoJpaRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,6 +24,12 @@ class DemoApplicationTests {
 
   @Autowired
   private MockMvc mockMvc;
+  @Autowired
+  private TodoJpaRepository TodoJpaRepository;
+  @AfterEach
+  void cleanDatabase() {
+    TodoJpaRepository.deleteAll();
+  }
 
   @Test
   void should_list_all_todo_when_none_todo_then_return_empty() throws Exception {
@@ -84,5 +95,39 @@ class DemoApplicationTests {
         .content(requestBody))
       .andExpect(status().isUnprocessableEntity());
   }
+
+  @Test
+  void should_put_todo_when_put_todo_then_return_updated() throws Exception {
+    String requestBody = """
+                 {
+                    "text":"third todo",
+                    "completed":false
+      
+                   }
+      """;
+
+    ResultActions perform = mockMvc.perform(post("/todos")
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(requestBody));
+
+    String updateRequestBody = """
+                 {
+                    "text":"third todo updated",
+                    "completed":true
+      
+                   }
+      """;
+
+    mockMvc.perform(put("/todos/{id}", 1)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(updateRequestBody))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.text").value("third todo updated"))
+      .andExpect(jsonPath("$.completed").value(true));
+  }
+
+
+
+
 
 }
